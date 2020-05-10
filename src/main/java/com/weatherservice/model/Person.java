d1c16025b78a79609f8a97886a8bd3f34ef802f5
Sabine Suhrati <sabine.suhrati@accenture.com>
2020-05-10 16:05:43 +0300

    add style to pages

--- src/main/java/com/weatherservice/model/Person.java
+++ src/main/java/com/weatherservice/model/Person.java
@@ -1,5 +1,7 @@
 package com.weatherservice.model;
 
+import com.google.gson.Gson;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Entity
 @Table(name="person")
@@ -53,5 +61,19 @@ public class Person {
 
     public void setCity(String city) {
         this.city = city;
+    }
+
+    public double tempInCity() throws IOException {
+        String apiKey = "df04ad59a46e9e3ffb04df4178548f9c";
+        URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+        BufferedReader json  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        City result = new Gson().fromJson(json, City.class);
+
+        return result.getMain().temp;
     }
 }
