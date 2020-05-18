ca5824f2bb24e64c39ee36a1afbcad7d803bdf0b
Sabine Suhrati <sabine.suhrati@accenture.com>
2020-05-18 10:51:03 +0300

    fix test

--- src/test/java/com/weatherservice/scheduling/ScheduledTasksTests.java
+++ src/test/java/com/weatherservice/scheduling/ScheduledTasksTests.java
@@ -1,12 +1,31 @@
 package com.weatherservice.scheduling;
 
+import com.weatherservice.service.WeatherService;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
+
 public class ScheduledTasksTests {
+    @InjectMocks
+    ScheduledTasks scheduledTasks;
+
+    @Mock
+    WeatherService weatherService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void updateCacheTest() throws IOException {
-        new ScheduledTasks().updateCache();
+        when(weatherService.getRigaWeather()).thenReturn(null);
+        scheduledTasks.updateCache();
     }
 }
